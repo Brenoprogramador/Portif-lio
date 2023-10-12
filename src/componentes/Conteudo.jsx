935b90d951d64d1fdd15a32c3cd8ed3bddab6af6
Breno Nunes Souza <brenonunes@MacBook-Air-de-Breno.local>
2023-10-12 08:22:37 -0300

    andamento do portifólio

--- src/componentes/Conteudo.jsx
+++ src/componentes/Conteudo.jsx
@@ -23,11 +23,40 @@ export default function Conteudo (){
                     <img className='icones' src={imgJs} alt="" />
                     <img className='icones' src={imgHtml} alt="" />
                     <img className='icones' src={imgReact} alt="" />
-                    <img className='icones'src={imgNode} alt="" />
-                    <img className='icones'src="" alt="" />
-                    <img className='icones'src="" alt="" />
-                    <img className='icones'src="" alt="" />
-                    <img className='icones'src="" alt="" />   
+                    <img className='icones'src={imgNode} alt="" />  
+                </div>
+                <button className='botaoContato'>Contate-me!</button>
+            </section>
+            <section className='container-principal secundario'>
+                    <img className='fotoPessoal' src={fotoPessoal} alt="" />
+                <div className="apresentacao">
+                    <h1 className='tituloSobre'>Sobre mim</h1>
+                    <p className='textoPrincipal'>Meu nome é Breno Nunes Souza, e tenho a grande paixão pela tecnologia, particularmente pelo desenvolvimento web. Hoje, gostaria de compartilhar com vocês um pouco sobre minha jornada até agora, minhas habilidades e minha busca pelo meu primeiro emprego no campo do desenvolvimento web.
+                    Minha jornada me levou a me especializar em diversas tecnologias-chave. Uma das minhas maiores habilidades é o React, Javascript, HTML, CSS, Git, docker. 
+                    </p>
+                </div>
+            </section>
+            <section className='terceario'>
+                <h1 className='tituloProjetos'>Projetos</h1>
+                <div className="cardProjetos">
+                    <ul>
+                        <li>
+                            <img className='imgProjetos' src="" alt="" />
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facilis nemo quasi voluptate officiis reiciendis modi ad ea eaque exercitationem.
+                        </li>
+                        <li>
+                            <img className='imgProjetos'  src="" alt="" />
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facilis nemo quasi voluptate officiis reiciendis modi ad ea eaque exercitationem.
+                        </li>
+                        <li>
+                            <img className='imgProjetos' src="" alt="" />
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facilis nemo quasi voluptate officiis reiciendis modi ad ea eaque exercitationem.
+                        </li>
+                        <li>
+                            <img className='imgProjetos' src="" alt="" />
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur facilis nemo quasi voluptate officiis reiciendis modi ad ea eaque exercitationem.
+                        </li>
+                    </ul>
                 </div>
             </section>
             
